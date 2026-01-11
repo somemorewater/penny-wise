e5f809478a807ae9d05dca829826a17b1fb340a7
somemorewater <oboyiloejeh@gmail.com>
2026-01-11 12:00:21 +0100

    feat: Implement MongoDB connection setup

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db')
 
 dotenv.config();
 const app = express();
+connectDB();
 const PORT = process.env.PORT;
+
+
+app.use(express.json()); 
 
 
 app.listen(PORT, () => {

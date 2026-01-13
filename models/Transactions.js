aeaa5c4edb608c1ad7a2c8b9412a0470d25638c6
somemorewater <oboyiloejeh@gmail.com>
2026-01-13 08:54:31 +0100

    feat: Add Transaction model for handling income and expense records

---
+++ models/Transactions.js
@@ -0,0 +1,15 @@
+const mongose = require('mongoose');
+
+const TransactionSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    type: { type: String, enum: ["income", "expense"], required: true },
+    amount: { type: Number, required: true },
+    category: { type: String, required: true },
+    description: String,
+    date: { type: Date, default: Date.now },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Transaction", TransactionSchema);

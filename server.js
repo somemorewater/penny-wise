5d67bdeab68d2f2ae5ad5a398c6aef7a9e807716
somemorewater <oboyiloejeh@gmail.com>
2026-01-17 14:43:53 +0100

    add frontend url

--- server.js
+++ server.js
@@ -12,10 +12,11 @@ dotenv.config();
 const app = express();
 connectDB();
 const PORT = process.env.PORT;
+const FRONTEND_URL = process.env.FRONTEND_URL
 
 app.use(
   cors({
-    origin: "FRONTEND_URL",
+    origin: FRONTEND_URL,
     credentials: true,
   })
 );
